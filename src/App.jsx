769280d54bee692d0e85fadfe1436e902906c225
Facundo Veron <facu_ve_2016@hotmail.com>
2022-11-23 23:06:56 -0300

    Comenzando con Style Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Heading = styled.h1`
+	font-family: 'Lato', sans-serif;
+	color: #fff;
+`;
 
 function App() {
 	return (
 		<div>
-			<h1>Desde App</h1>
+			<Heading>Desde App</Heading>
 		</div>
 	);
 }
